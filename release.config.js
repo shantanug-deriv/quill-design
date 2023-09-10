e45c0230603986bbf455b7ad152c52a85142d667
Michio <mohsen@re-work.dev>
2023-09-10 16:08:17 +0330

    ci: enable release, removed dryRun flag from semantic release config

--- release.config.js
+++ release.config.js
@@ -4,7 +4,6 @@ const transformer = require('./release.utils.js')
 
 module.exports = {
 	branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', 'next', 'next-major', { name: 'beta', prerelease: true }, { name: 'alpha', prerelease: true }],
-	dryRun: true,
 	repositoryUrl: 'git@github.com:deriv-com/quill-design.git',
 	plugins: [
 		[
