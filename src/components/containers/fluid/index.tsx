55b251138b375f55fe4c84406d47ab444579d483
Michio <mohsen@re-work.dev>
2023-09-26 14:51:59 +0330

    chore: update media queires to use quill-tailwind screens

--- src/components/containers/fluid/index.tsx
+++ src/components/containers/fluid/index.tsx
@@ -8,7 +8,7 @@ export const FluidContainer = ({
   return (
     <div
       className={qtMerge(
-        'xl:px-0 max-w-screen-xl mx-auto w-full bg-solid-red-900 px-general-lg md:px-general-xl',
+        'xl:px-0 mx-auto w-full max-w-screen-xl bg-background-primary-container px-general-lg md:px-general-xl',
         className,
       )}
       {...rest}
