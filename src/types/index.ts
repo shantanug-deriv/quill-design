2e4241f46c3da63d028c0d15d0cbc66c305e4503
Michio <mohsen@re-work.dev>
2023-09-28 13:34:37 +0330

    chore: updated build exports
    - removed CJS export
    - now we're exporting quill-design.css for all the styles
    - removed box component

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,15 @@
 export type Theme = 'dark' | 'light'
+
+export type UnionToIntersection<U> = (
+  U extends unknown ? (k: U) => void : never
+) extends (k: infer I) => void
+  ? I
+  : never
+
+export type AsProp<T, P> = T extends keyof P
+  ? { as?: T; className?: string }
+  : never
+export type GenericElements<T extends keyof JSX.IntrinsicElements> = Pick<
+  JSX.IntrinsicElements,
+  T
+>
