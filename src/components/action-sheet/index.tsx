2fee24ba92950f3e11b8406512b3d2e722d79adb
hasan-deriv <abuhasan@deriv.com>
2023-11-03 16:44:37 +0800

    feat: added wrapper classes

--- src/components/action-sheet/index.tsx
+++ src/components/action-sheet/index.tsx
@@ -1,10 +1,15 @@
+import qtMerge from 'qtMerge'
 import Content from './content'
 import Footer from './footer'
 import Header from './header'
 
 const ActionSheet = () => {
   return (
-    <div>
+    <div
+      className={qtMerge(
+        'h-auto w-full rounded-t-800 bg-background-dialog px-800 pb-800',
+      )}
+    >
       <Header>Header</Header>
       <Content>Content</Content>
       <Footer>Footer</Footer>
