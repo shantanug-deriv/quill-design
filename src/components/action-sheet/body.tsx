aaf32418db9f799550ff9b26808d90cef53d6508
hasan-deriv <abuhasan@deriv.com>
2023-11-03 17:01:51 +0800

    fix: removed body

--- src/components/action-sheet/body.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentPropsWithoutRef } from 'react'
-
-type BodyProps = ComponentPropsWithoutRef<'div'>
-
-const Body = ({ children }: BodyProps) => {
-  return <div>{children}</div>
-}
-
-export default Body
