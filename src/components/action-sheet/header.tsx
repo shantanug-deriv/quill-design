6f983a60b51bd58cf31a0972b3ff304babc4ad2e
hasan-deriv <abuhasan@deriv.com>
2023-11-06 11:26:02 +0800

    chore: header

--- src/components/action-sheet/header.tsx
+++ src/components/action-sheet/header.tsx
@@ -1,6 +1,11 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ForwardRefExoticComponent } from 'react'
+import { QuillSvgProps } from '@deriv/quill-icons'
 
-type HeaderProps = ComponentPropsWithoutRef<'div'>
+type HeaderProps = ComponentPropsWithoutRef<'div'> & {
+  title?: string
+  icon?: ForwardRefExoticComponent<Omit<QuillSvgProps, 'ref'>>
+  description?: string
+}
 
 const Header = ({ children }: HeaderProps) => {
   return <div>{children}</div>
