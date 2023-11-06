46513de424c2caef704f5e4a53e07e7dd2ff0575
hasan-deriv <abuhasan@deriv.com>
2023-11-06 17:28:35 +0800

    chore: added actions to footer

--- src/components/action-sheet/footer.tsx
+++ src/components/action-sheet/footer.tsx
@@ -1,10 +1,54 @@
 import { ComponentPropsWithoutRef } from 'react'
 import qtMerge from 'qtMerge'
+import { Button } from '../button'
 
-type FooterProps = ComponentPropsWithoutRef<'div'>
+type ActionType = {
+  content: string
+  onAction: () => void
+}
 
-const Footer = ({ children }: FooterProps) => {
-  return <div className={qtMerge('flex md:mt-auto')}>{children}</div>
+type FooterProps = ComponentPropsWithoutRef<'div'> & {
+  primaryAction?: ActionType
+  secondaryAction?: ActionType
+  alignment?: 'vertical' | 'horizontal'
+}
+
+const Footer = ({
+  primaryAction,
+  secondaryAction,
+  alignment = 'vertical',
+}: FooterProps) => {
+  return (
+    <div
+      className={qtMerge(
+        'flex gap-400 pt-800 md:mt-auto',
+        alignment === 'vertical' && 'flex-col',
+        alignment === 'horizontal' && 'flex-row',
+      )}
+    >
+      {primaryAction && (
+        <Button
+          onClick={primaryAction.onAction}
+          colorStyle="black"
+          size="lg"
+          fullWidth
+        >
+          {primaryAction.content}
+        </Button>
+      )}
+      {secondaryAction && (
+        <Button
+          onClick={secondaryAction.onAction}
+          variant="secondary"
+          colorStyle="black"
+          size="lg"
+          fullWidth
+        >
+          {secondaryAction.content}
+        </Button>
+      )}
+    </div>
+  )
 }
 
 export default Footer
