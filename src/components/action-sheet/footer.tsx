d9682c1bdec524a0f61a1845098ad2ed479f4e24
hasan-deriv <abuhasan@deriv.com>
2023-11-06 14:27:12 +0800

    chore: added side sheet

--- src/components/action-sheet/footer.tsx
+++ src/components/action-sheet/footer.tsx
@@ -4,7 +4,7 @@ import qtMerge from 'qtMerge'
 type FooterProps = ComponentPropsWithoutRef<'div'>
 
 const Footer = ({ children }: FooterProps) => {
-  return <div className={qtMerge('flex')}>{children}</div>
+  return <div className={qtMerge('flex md:mt-auto')}>{children}</div>
 }
 
 export default Footer
--- src/components/action-sheet/root.tsx
+++ src/components/action-sheet/root.tsx
@@ -4,14 +4,25 @@ import qtMerge from 'qtMerge'
 type RootProps = ComponentPropsWithoutRef<'div'> & {
   show: boolean
   onClose: () => void
+  position?: 'left' | 'right'
 }
 
-const Root = ({ children, className, show, onClose }: RootProps) => {
+const Root = ({
+  children,
+  className,
+  show,
+  onClose,
+  position = 'right',
+}: RootProps) => {
   return (
     <div
       className={qtMerge(
         'invisible fixed inset-general-none -bottom-full z-10 flex items-end justify-center opacity-50 transition-all',
         show && 'visible bottom-50 opacity-1300',
+        position === 'right' && 'md:-right-full md:bottom-50',
+        position === 'left' && 'md:-left-full md:bottom-50',
+        show && position === 'right' && 'md:right-50',
+        show && position === 'left' && 'md:left-50',
       )}
     >
       <div
@@ -20,7 +31,9 @@ const Root = ({ children, className, show, onClose }: RootProps) => {
       ></div>
       <div
         className={qtMerge(
-          'min-w-[320px] max-w-[800px] rounded-t-800 bg-background-dialog px-800 pb-800',
+          'flex min-w-[320px] max-w-[800px] flex-col rounded-t-800 bg-background-dialog px-800 pb-800 md:h-full md:max-w-[360px] md:rounded-50',
+          position === 'right' && 'md:ml-auto',
+          position === 'left' && 'md:mr-auto',
           className,
         )}
       >
