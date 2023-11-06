596492f07b4f783c4ddc78e3822fd173a2973431
hasan-deriv <abuhasan@deriv.com>
2023-11-06 14:04:50 +0800

    chore: added icon to handle bar

--- src/components/action-sheet/handle-bar.tsx
+++ src/components/action-sheet/handle-bar.tsx
@@ -1,5 +1,17 @@
 const HandleBar = () => {
-  return <div>HandleBar</div>
+  return (
+    <div className="flex items-center justify-center py-400">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="4"
+        viewBox="0 0 48 4"
+        fill="none"
+      >
+        <rect width="48" height="4" rx="2" fill="black" fillOpacity="0.08" />
+      </svg>
+    </div>
+  )
 }
 
 export default HandleBar
