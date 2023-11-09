520d1a658c86a0f45d180b97b83507ebe30487b7
hasan-deriv <abuhasan@deriv.com>
2023-11-09 15:16:25 +0800

    chore: added portal

--- src/components/action-sheet/root/index.tsx
+++ src/components/action-sheet/root/index.tsx
@@ -1,3 +1,4 @@
+import { createPortal } from 'react-dom'
 import qtMerge, { qtJoin } from 'qtMerge'
 import HandleBar from '../handle-bar'
 import { actionSheetRootCVA } from '../action-sheet.classnames'
@@ -19,32 +20,39 @@ const Root = ({
   //  TODO: need to update the transition classes
   return (
     <>
-      {type === 'modal' && (
-        <div
-          data-testid="modal-overlay"
-          onClick={onClose}
-          className={qtJoin(
-            'fixed inset-50 z-10 bg-opacity-black-500 transition-opacity duration-[160ms] ease-[cubic-bezier(0.72,_0,_0.24,_1)]',
-            !show && 'invisible',
+      {createPortal(
+        <>
+          {type === 'modal' && (
+            <div
+              data-testid="modal-overlay"
+              onClick={onClose}
+              className={qtJoin(
+                'fixed inset-50 z-10 bg-opacity-black-500 transition-opacity duration-[160ms] ease-[cubic-bezier(0.72,_0,_0.24,_1)]',
+                !show && 'invisible',
+              )}
+            ></div>
           )}
-        ></div>
+          <div
+            className={qtJoin(
+              'pointer-events-none fixed inset-general-none z-10 flex select-none items-end justify-center overflow-x-hidden transition-all duration-[160ms] ease-[cubic-bezier(0.72,_0,_0.24,_1)]',
+              !show && 'invisible',
+            )}
+            {...restProps}
+          >
+            <div
+              className={qtMerge(
+                actionSheetRootCVA({ show, position, className }),
+              )}
+              ref={containerRef}
+              style={{ height }}
+            >
+              {expandable && <HandleBar {...bindHandle()} />}
+              {children}
+            </div>
+          </div>
+        </>,
+        document.body,
       )}
-      <div
-        className={qtJoin(
-          'pointer-events-none fixed inset-general-none z-10 flex select-none items-end justify-center overflow-x-hidden transition-all duration-[160ms] ease-[cubic-bezier(0.72,_0,_0.24,_1)]',
-          !show && 'invisible',
-        )}
-        {...restProps}
-      >
-        <div
-          className={qtMerge(actionSheetRootCVA({ show, position, className }))}
-          ref={containerRef}
-          style={{ height }}
-        >
-          {expandable && <HandleBar {...bindHandle()} />}
-          {children}
-        </div>
-      </div>
     </>
   )
 }
