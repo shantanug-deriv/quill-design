59745e763e1910fec6f76db54ec304a9ec5ffb63
prince-deriv <prince@deriv.com>
2023-11-13 14:59:00 +0400

    fix: elevate story rendering size

--- src/components/accordion/elevate/elevate.stories.tsx
+++ src/components/accordion/elevate/elevate.stories.tsx
@@ -67,7 +67,7 @@ export const LongSubtitle: Story = {
     title: 'Accordion Elevate (Long Subtitle)',
     subtitle:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    className: 'w-[350px]',
+    className: '!w-[350px]',
     content: () => (
       <img
         className="flex w-full flex-1"
